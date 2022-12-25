5af44c3eb4ed9f526130f98408acbb9530ab6b31
medanisjbara <anis2834133766619@gmail.com>
2022-12-25 23:04:50 +0100

    skip if no change + Code cleanup

--- extrafetcher.py
+++ extrafetcher.py
@@ -1,4 +1,3 @@
-# Imports
 from PIL import Image
 from pdf2image import convert_from_bytes
 import json, os, requests, io
@@ -9,49 +8,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-text_pages = os.listdir('text_pages') if os.path.exists('text_pages') else []
 
-# User data
-username = ''
-password = ''
-# Link to the link that will be scraped after login
-# Example: "https://fsm.rnu.tn/fra/intranet/news/etu.view/358"
-devoir_url = ""
-if os.path.exists('credentials.json'):
-    creds = json.load(open('credentials.json'))
-    username = creds['username']
-    password = creds['password']
-    devoir_url = creds['url']
-
-
-def fetch_update():
-    # init selenium browser
+def browser_init(headless=True):
+    """
+    Initialises the browser
+    """
     opts = webdriver.FirefoxOptions()
-    opts.headless = True
+    opts.headless = headless
     driver = webdriver.Firefox(options=opts)
-    driver.get("https://fsm.rnu.tn//extranet")
-    
-    # init directories
-    for folder in ['images', 'text_pages']:
-      if os.path.exists(folder):
-        for file in os.listdir(folder):
-          os.remove(folder+'/'+file)
-        os.rmdir(folder)
-      os.mkdir(folder)
+    return driver
 
-    # Ask for username and password
-    global username, password, devoir_url
-    if not username:
-      username = input('Enter extranet\'s username: ')
-    if not password:
-      password = input('Enter extranet\'s password: ')
-    if not devoir_url:
-      print('Enter the link of the page containing the files')
-      print('Example: https://fsm.rnu.tn/fra/intranet/news/etu.view/358')
-      devoir_url = input('URL: ')
-    json.dump({'username':username, 'password': password, 'url':devoir_url}, open('credentials.json','w'))
-    
-    # Login
+def extranet_login(username, password, driver):
+    """
+    Automates the login process to https://fsm.rnu.tn/extranet
+    Works by using pytesseract on the captcha.
+
+    Note that this function does this in a while loop. Make sure to provide
+    the correct credentials
+    """
+    driver.get("https://fsm.rnu.tn//extranet")
     while True:
       ## Fields to fill
       username_field = driver.find_element(By.NAME, "username")
@@ -71,43 +46,13 @@ def fetch_update():
       username_field.send_keys(username)
       password_field.send_keys(password)
       captcha_field.send_keys(captcha_text)
-    
+      
       ## Actual login
       driver.find_element(By.CLASS_NAME, "button").click()
+
     
       if driver.current_url != "https://fsm.rnu.tn/fra/intranet/auth/wrong-auth":
         break
-    
-    driver.get(devoir_url)
-    
-    for e in driver.find_elements(By.XPATH, '/html/body/div/div/div/div/span/*'):
-        if e.tag_name == 'ul':
-            for li in e.find_elements(By.XPATH, './*'):
-                if li.tag_name != 'li':
-                    print(li.tag_name, "found where it shouldn't be")
-                    print("Manual intervention is needed")
-                else:
-                    seance = driver.execute_script('return arguments[0].outerHTML', li)
-                    for devoir in seance[seance.find('(') +1 : seance.find(')')].split(', '):
-                        pdf = devoir[devoir.find('"')+1:]
-                        pdf = pdf[:pdf.find('"')]
-                        process_pdf(pdf)
-            break
-        elif e.tag_name == 'div':
-            for tag in e.find_elements(By.XPATH, './*'):
-                if tag.tag_name == 'ul':
-                    print("Found nested tags")
-                    for li in tag.find_elements(By.XPATH, './*'):
-                        if li.tag_name == 'li':
-
-                            seance = driver.execute_script('return arguments[0].outerHTML', li)
-                            for devoir in seance[seance.find('(') +1 : seance.find(')')].split(', '):
-                                pdf = devoir[devoir.find('"')+1:]
-                                pdf = pdf[:pdf.find('"')]
-                                process_pdf(pdf)
-                        else:
-                            print('(nested)',li.tag_name, "found where it shouldn't be")
-                            print("Manual intervention is needed")
 
 def process_pdf(pdf):
     image_bytes = requests.get('https://fsm.rnu.tn'+pdf).content
@@ -133,19 +78,44 @@ def prepare_image(image,name, darkmode=True):
     Image.fromarray(img_arr).save('images/'+name)
     print('Done!\n')
 
+def scrape_source(source, driver):
+    def parse_ul(e):
+        for li in e.find_elements(By.XPATH, './*'):
+            if li.tag_name != 'li':
+                print(li.tag_name, "found where it shouldn't be")
+                print("Manual intervention is needed")
+                continue
+            for a in li.find_elements(By.XPATH, './*'):
+                process_pdf(driver.execute_script('return arguments[0].getAttribute("href")', a))
+    html_bs64 = base64.b64encode(source.encode('utf-8')).decode()
+    driver.get("data:text/html;base64," + html_bs64)
+    for e in driver.find_elements(By.XPATH, '/html/body/div/div/div/div/span/*'):
+        if e.tag_name not in ['ul', 'div']:
+            continue
+        if e.tag_name == 'ul':
+                parse_ul(e)
+        for tag in e.find_elements(By.XPATH, './*'):
+            if tag.tag_name == 'ul':
+                print("Found nested tags")
+                parse_ul(e)
 
-if __name__=='__main__':
-    if len(argv) != 2:
-        print('Usage: '+argv[0]+' [fetch|find]')
-        print('\tfetch:\tgrabs pdf files from FSM website')
-        print('\tfind:\tfinds your class based on your CIN')
-    else:
-        if argv[1] == 'fetch':
-            fetch_update()
-        elif argv[1] == 'find':
-            x = get_user_output(input('CIN: '))
-            print(x)
-        else:
-            print('Usage: '+argv[0]+' [fetch|find]')
-            print('\tfetch:\tgrabs pdf files from FSM website')
-            print('\tfind:\tfinds your class based on your CIN')
+def fetch_update(username, password, target):
+    driver = browser_init()
+    extranet_login(username, password, driver)
+    driver.get(target)
+    source = driver.page_source
+    if not os.path.exists('local-source.html'):
+        open('local-source.html','w')
+    elif source == open('local-source.html').read():
+        return
+    open('local-source.html', 'w').write(source)
+
+    # init directories
+    for folder in ['images', 'text_pages']:
+      if os.path.exists(folder):
+        for file in os.listdir(folder):
+          os.remove(folder+'/'+file)
+        os.rmdir(folder)
+      os.mkdir(folder)
+
+    scrape_source(source, driver)
