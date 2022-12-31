cf2992756ad4c3f48c8a38e388a21ac3cf45b627
medanisjbara <anis2834133766619@gmail.com>
2022-12-31 01:42:08 +0100

    closes #3

--- webapp.py
+++ webapp.py
@@ -1,22 +1,15 @@
-from flask import Flask, request, redirect, send_file
+from flask import Flask, request, redirect, send_file, render_template
 from flask_cors import CORS, cross_origin
 
 import os
 
-wrap_up = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body style="background-color: black; ">'
-wrap_down = '</body></html>'
-
-suc_msg = '<h1 style="color: white; text-align: center; font-size: 5em;">Here is what you are looking for</h1>'
-
-def get_user_output(cin):
-    def get_html(page):
-        return '<img src="/images/'+ page + '.png" style="width:40%;display: block; margin-left: auto; margin-right: auto; ">'
-    user_output = []
+def get_cropped_docs(cin):
+    needed_images = []
     text_pages = os.listdir('text_pages') if os.path.exists('text_pages') else []
     for text_page in text_pages:
         if cin in open('text_pages/'+text_page).read():
-            user_output.append(get_html(text_page.replace('.txt','')))
-    return '<br/>'.join(user_output)
+            needed_images.append(text_page[:-3]+'png')
+    return needed_images
 
 
 
@@ -24,15 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return '''
-
-<body style="background-color: black; ">'
-<form method="POST" action="/find">
-  <input name="cin" id="cin" type="text">
-  <input type="submit">
-</form>
-</body>
-    '''
+    return render_template('search.html')
 
 @app.route('/find', methods=['POST', 'GET'])
 def find():
@@ -40,10 +25,9 @@ def find():
         cin = request.get_data().decode().split('=')[1]
         if not cin:
             return redirect('/')
-        user_output = get_user_output(cin)
-        if user_output:
-          # TODO: Use flask specific syntax to generate html from template
-          return wrap_up + suc_msg + user_output + wrap_down
+        needed_images = get_cropped_docs(cin)
+        if needed_images:
+            return render_template('result.html',docs=needed_images, len=len(needed_images)) 
         return 'No results from your querry, please try again', {"Refresh": "1; url=/"}
     return redirect ('/')
 
@@ -56,5 +40,5 @@ def get_image(image_name):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
 
