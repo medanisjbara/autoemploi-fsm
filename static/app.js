08375bbaf9f6a3d62ec640ef186d994cee46747f
rayenzaafouri <mohamedrayenzaafouri@gmail.com>
2023-03-12 19:44:59 +0100

    Minor bugfixes

--- static/app.js
+++ static/app.js
@@ -105,9 +105,7 @@ document.addEventListener("load", (event)=>{
     let image_elements = document.getElementsByClassName("result-img");
     for (let i=0;i<image_elements.length;i++){
         image_elements[i].addEventListener("click",(event)=>{
-            image_elements[i].addEventListener("click",(event)=>{
                 display_image_fs(image_elements[i].getAttribute("src"));
-            })
         });
     }
 });
